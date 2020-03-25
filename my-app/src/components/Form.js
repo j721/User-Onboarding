b8621489fc62dc7ea9e26e67a13cbcd58d356b9a
j721 <juliadvu@gmail.com>
2020-03-25 15:45:28 -0500

    More changes made to form.js file

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import * as yup from "yup";
 import axios from 'axios';
 
@@ -20,8 +20,8 @@ function Form () {
         positions: '',
 
     });
-//state for errors
 
+//state for errors
 const [errors, setErrors]= useState({
     name: '',
     email: '',
@@ -35,11 +35,94 @@ const [buttonDisabled, setButtonDisabled] = useState(true);
 //state for post requests
 const [post, setPost] = useState([]);
 
+useEffect(()=>{
+    formSchema.isValid(formState).then(valid=>{
+        setButtonDisabled(!valid);
+    });
+},[formState]);
+
+const validateChange = event =>{
+    yup.reach(formState, event.target.name)
+    .validate(event.target.value)
+    .then(valid=>{
+        setErrors({
+            ...errors, [event.target.name]:""
+        });
+    })
+    .catch(err=>{
+        setErrors({
+            ...errors, [event.target.name]: err.errors
+        });
+    });
+}
+
+const formSubmit =event =>{
+    event.preventDefault();
+    axios.post(" https://reqres.in/api/users", formState)
+    .then(response=>{
+        setPost(response.data);
+        console.log('received data', post);
+    })
+    .catch(error=>{
+        console.log(error.response)
+    });
+}
+
+const inputChange = event =>{
+    event.persist();
+    const newFormData ={...formState, [event.target.name]: 
+    event.target.type ==="checkbox" ? event.target.checked: event.target.value
+    };
+    validateChange(event);
+    setFormState(newFormData);
+}
 
 return (
-        <div>
+       <form onSubmit ={formSubmit}>
+           <label htmlFor ='name'>Name  
+                <input 
+                id ="name"
+                type="text"
+                name= "name"
+                value ={formState.name}
+                onChange={inputChange}
+                />
+                {errors.name.length > 0 ? <p className="error">{errors.name}</p>:null}
+           </label>
 
-        </div>
+           <label htmlFor ='email'>Email  
+                <input 
+                id ="email"
+                type="text"
+                name= "email"
+                value ={formState.email}
+                onChange={inputChange}
+                />
+                {errors.email.length > 0 ? <p className="error">{errors.email}</p>:null}
+           </label>
+
+           <label htmlFor ='password'>Password
+                <input 
+                id ="password"
+                type="text"
+                name= "password"
+                value ={formState.password}
+                onChange={inputChange}
+                />
+                {errors.password.length > 0 ? <p className="error">{errors.password}</p>:null}
+           </label>
+
+           <label htmlFor="terms" className="terms"> Terms and Conditions
+                <input
+                type="checkbox"
+                name="terms"
+                checked ={formState.terms}
+                onChange={inputChange}
+                />
+           </label>
+           <pre>{JSON.stringify(post, null, 2)}</pre>
+           <button disabled ={buttonDisabled}>Submit</button>
+       </form>
  )
 }
 
